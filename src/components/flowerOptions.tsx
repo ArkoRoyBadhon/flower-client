4c357c0cd043bffb4661d7f4ed725f9dfed91cc2
Arko <arkoroyb@gmail.com>
2024-01-26 02:05:27 +0600

    add flower form created and react-datepicker, react-select installed

---
+++ src/components/flowerOptions.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
+export const colorOptions:any = [
+    { value: "red", label: "Red" },
+    { value: "white", label: "White" },
+    { value: "yellow", label: "Yellow" },
+    { value: "orange", label: "Orange" },
+    { value: "pink", label: "Pink" },
+    { value: "blue", label: "Blue" },
+    { value: "purple", label: "Purple" },
+  ];
+
+export const typeOptions:any = [
+    { value: "rose", label: "Rose" },
+    { value: "lily", label: "Lily" },
+    { value: "tulip", label: "Tulip" },
+    { value: "daisy", label: "Daisy" },
+    { value: "sunflower", label: "Sunflower" },
+    { value: "orchid", label: "Orchid" },
+    { value: "carnation", label: "Carnation" },
+  ];
+
+
+export const sizeOptions:any = [
+    { value: "small", label: "Small" },
+    { value: "medium", label: "Medium" },
+    { value: "large", label: "Large" },
+  ];
+
+  export const fragranceOptions:any = [
+    { value: "rose-scented", label: "Rose-scented" },
+    { value: "mild", label: "Mild" },
+    { value: "sweet", label: "Sweet" },
+    { value: "none", label: "None" },
+    { value: "floral", label: "Floral" },
+    { value: "earthy", label: "Earthy" },
+    { value: "light", label: "Light" },
+    { value: "orchid-scented", label: "Orchid-scented" },
+    { value: "fruity", label: "Fruity" },
+  ];
+  
+  export const occationOptions:any = [
+    { value: "anniversary", label: "Anniversary" },
+    { value: "birthday", label: "Birthday" },
+    { value: "wedding", label: "Wedding" },
+    { value: "valentines Day", label: "Valentines Day" },
+    { value: "cristmas", label: "Cristmas" },
+    { value: "mother Language Day", label: "Mother Language Day" },
+    { value: "independence Day", label: "Independence Day" },
+    { value: "pohela Boishak", label: "Pohela Boishak" },
+  ];
+
--- src/pages/AddFlower.tsx
+++ src/pages/AddFlower.tsx
@@ -1,10 +1,200 @@
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import Select from "react-select";
+import {
+  colorOptions,
+  fragranceOptions,
+  occationOptions,
+  sizeOptions,
+  typeOptions,
+} from "../components/flowerOptions";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+export type IFlower = {
+  name: string;
+  img: string;
+  price: string;
+  quantity: number;
+  bloom_date: string;
+  color: string;
+  type: string;
+  size: string;
+  fragrance: string;
+  occation: string;
+};
 
 const AddFlower = () => {
-    return (
-        <div>
-            AddFlower
-        </div>
-    );
+  const {
+    control,
+    register,
+    handleSubmit,
+    // reset,
+    formState: { errors },
+  } = useForm<IFlower>();
+  const onSubmit: SubmitHandler<IFlower> = async (data) => {
+    console.log("form data", data);
+    // reset()
+  };
+
+  return (
+    <div className="p-5">
+      <h1 className="text-[18px] font-bold text-green w-full">Add Flower</h1>
+      <div className="mt-10 w-full flex justify-center">
+        <form className="w-full md:w-[60%] lg:w-[40%] shadow-md p-10" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col mb-2">
+            <label htmlFor="">Name</label>
+            <input
+              className="py-[6px] px-2 mx-auto rounded-md w-full border border-gray-300"
+              type="text"
+              placeholder="name"
+              {...register("name", { required: true })}
+            />
+            {errors.name && (
+              <span className="text-center text-red-500">
+                This field is required
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <label htmlFor="">Image</label>
+            <input
+              className="py-[6px] px-2 mx-auto rounded-md w-full border border-gray-300"
+              type="file"
+              placeholder="Image"
+              {...register("img", { required: true })}
+            />
+            {errors.img && (
+              <span className="text-center text-red-500">
+                This field is required
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <label htmlFor="">Price</label>
+            <input
+              className="py-[6px] px-2 mx-auto rounded-md w-full border border-gray-300"
+              type="text"
+              placeholder="Price"
+              {...register("price", { required: true })}
+            />
+            {errors.price && (
+              <span className="text-center text-red-500">
+                This field is required
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <label htmlFor="">Quantity</label>
+            <input
+              className="py-[6px] px-2 mx-auto rounded-md w-full border border-gray-300"
+              type="text"
+              placeholder="Quantity"
+              {...register("quantity", { required: true })}
+            />
+            {errors.quantity && (
+              <span className="text-center text-red-500">
+                This field is required
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col mb-2">
+            <label htmlFor="">Bloom Date</label>
+            <div className="py-[6px] px-2 mx-auto rounded-md w-full border border-gray-300">
+              <Controller
+                control={control}
+                name="bloom_date"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <DatePicker
+                    showIcon
+                    minDate={new Date()}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    selected={value ? new Date(value) : null}
+                  />
+                )}
+              />
+            </div>
+          </div>
+
+          <div className="">
+            <label htmlFor="">Color</label>
+            <div className="">
+              <Controller
+                control={control}
+                name="color"
+                render={({ field }) => (
+                  <Select options={colorOptions} {...field} />
+                )}
+              />
+            </div>
+          </div>
+
+          <div className="">
+            <label htmlFor="">Type/Category</label>
+            <div className="">
+              <Controller
+                control={control}
+                name="type"
+                render={({ field }) => (
+                  <Select isMulti options={typeOptions} {...field} />
+                )}
+              />
+            </div>
+          </div>
+
+          <div className="">
+            <label htmlFor="">Size</label>
+            <div className="">
+              <Controller
+                control={control}
+                name="size"
+                render={({ field }) => (
+                  <Select isMulti options={sizeOptions} {...field} />
+                )}
+              />
+            </div>
+          </div>
+          <div className="">
+            <label htmlFor="">Fragrance</label>
+            <div className="">
+              <Controller
+                control={control}
+                name="fragrance"
+                render={({ field }) => (
+                  <Select isMulti options={fragranceOptions} {...field} />
+                )}
+              />
+            </div>
+          </div>
+          <div className="">
+            <label htmlFor="">Occation</label>
+            <div className="">
+              <Controller
+                control={control}
+                name="occation"
+                render={({ field }) => (
+                  <Select isMulti options={occationOptions} {...field} />
+                )}
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col mt-5">
+            <input
+              className="bg-deeper py-2 rounded-md cursor-pointer text-white mx-auto w-full md:w-[60%]"
+              type="submit"
+              value="Add Tutor"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddFlower;
