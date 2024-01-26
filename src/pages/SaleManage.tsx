b8f9fdbc135e0ce422d4b305ef6071575c1b709d
Arko <arkoroyb@gmail.com>
2024-01-26 23:27:23 +0600

    manage flower table creation and view functionality

--- src/pages/SaleManage.tsx
+++ src/pages/SaleManage.tsx
@@ -1,10 +1,69 @@
+import { Link } from "react-router-dom";
+import { useGetAllFlowerQuery } from "../redux/features/flower/flowerApi";
 
 const SaleManage = () => {
-    return (
-        <div>
-            SaleManage
+  const {
+    data: flowerData,
+    isLoading,
+    isSuccess,
+    error,
+  } = useGetAllFlowerQuery(undefined);
+
+  console.log("data", flowerData?.data?.data);
+
+  return (
+    <div className="p-5">
+      <h1 className="text-[18px] font-bold text-green w-full">Manage Sale</h1>
+      <div className="">
+        <div className="">
+          <h5 className="">Filter</h5>
         </div>
-    );
+        <hr />
+        <div className="mt-5 grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-y-10 md:gap-x-10 gap-x-0">
+          {flowerData?.data?.data?.map((item) => {
+            return (
+              <div
+                key={item?._id}
+                className="w-[300px] h-fit shadow-md rounded-md "
+              >
+                <div className="w-full h-[50%] overflow-hidden">
+                  <img
+                    className="rounded-t-md h-full w-full transition-transform transform hover:scale-110"
+                    src="https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
+                    alt="img"
+                  />
+                </div>
+                <div className="p-3 text-[16px]">
+                  <p className="font-semibold">{item?.name}</p>
+                  <p className="">
+                    <span className="font-semibold">Price: </span> {item?.price}
+                  </p>
+                  <p className="">
+                    <span className="font-semibold">Color: </span> {item?.color}
+                  </p>
+                  <p className="">
+                    <span className="font-semibold">Quantity: </span>
+                    {item?.quantity}
+                  </p>
+                  <div className="bg-gray-200 hover:bg-green px-3 py-1 mt-3 rounded-md w-full hover:text-white transition-all ease-in text-center">
+                    Update
+                  </div>
+                  <Link to={`/single-service/${item?._id}`} className="">
+                    <div className="bg-gray-200 hover:bg-green px-3 py-1 mt-3 rounded-md w-full hover:text-white transition-all ease-in text-center">
+                      View
+                    </div>
+                  </Link>
+                  <div className="bg-gray-200 hover:bg-green px-3 py-1 mt-3 rounded-md w-full hover:text-white transition-all ease-in text-center">
+                    Delete
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SaleManage;
--- src/pages/ViewFlower.tsx
+++ src/pages/ViewFlower.tsx
@@ -1,9 +1,161 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { useState } from "react";
+import { useGetAllFlowerQuery } from "../redux/features/flower/flowerApi";
+// import EditModals from "../modals/editModals";
+
 const ViewFlower = () => {
+  // manage user
+  const [Dmodal, setDModal] = useState(false);
+  const [DmodalData, setDModalData] = useState<any>();
+
+  // edit
+  const [editUser, setEditUser] = useState(false);
+  const [EUser, setEUser] = useState<any>();
+
+  const {
+    data: AllFlower,
+    isLoading,
+    isSuccess,
+  } = useGetAllFlowerQuery(undefined);
+  // const [deleteUser, { isSuccess: isSuccessError, error }] =
+  //   useDeleteUserMutation();
+  // const [updateUser, { isSuccess: isSuccessUpdate, error: errorUpdate }] =
+  //   useUpdateUserMutation();
+
+  const detailModal = (
+    <div className="fixed top-[100px] h-[400px] bg-gray-200 shadow-md w-[400px] rounded-md overflow-hidden overflow-y-auto">
+      <div className="flex justify-end">
+        <div
+          className="cursor-pointer
+        px-2 py-1 bg-red-300 w-fit"
+          onClick={() => {
+            setDModal(false);
+          }}
+        >
+          close
+        </div>
+      </div>
+      <div className="scroll-m-0">
+        <h3 className="text-center font-bold underline text-lg">Flower Detail</h3>
+        <div className="px-5 py-5 capitalize">
+          <img className="rounded-md" src={DmodalData?.img} alt="flower_img" />
+          <p className="mt-5">
+            <span className="font-semibold">Name: </span>
+            {DmodalData?.name}
+          </p>
+          <p className="">
+            <span className="font-semibold">Price: </span>
+            {DmodalData?.price}
+          </p>
+          <p className="">
+            <span className="font-semibold">Available Quantity: </span>
+            {DmodalData?.quantity}
+          </p>
+          <p className="">
+            <span className="font-semibold">Bloom Date: </span>
+            {DmodalData?.bloom_date}
+          </p>
+          <p className="">
+            <span className="font-semibold">Color: </span>
+            {DmodalData?.color}
+          </p>
+          <p className="">
+            <span className="font-semibold">Type/Category: </span>
+            {DmodalData?.type}
+          </p>
+          <p className="">
+            <span className="font-semibold">Size: </span>
+            {DmodalData?.size}
+          </p>
+          <p className="">
+            <span className="font-semibold">Fragrance: </span>
+            {DmodalData?.fragrance}
+          </p>
+          <p className="">
+            <span className="font-semibold">Occation: </span>
+            {DmodalData?.occation}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const handleDelUser = (id: string) => {
+    // deleteUser({ id });
+  };
+
+  // if (isSuccessError) {
+  //   console.log("delete success");
+  // }
+
+  // if (error) {
+  //   console.log("error", (error as any)?.data.message);
+  // }
+
+  // if (errorUpdate) {
+  //   console.log("error", (errorUpdate as any)?.data.message);
+  // }
+
   return (
-    <div className="p-5">
-      <h1 className="text-[18px] font-bold text-green w-full">Manage Flower</h1>
-      <div className="">
-        
+    <div className="p-10 relative">
+      <h2 className="text-center font-bold underline text-xl">
+        Manage Flowers
+      </h2>
+
+      {Dmodal && detailModal}
+      {/* {editUser && <EditModals EUser={EUser} setEditUser={setEditUser} />} */}
+
+      <div className="overflow-x-auto">
+        <table className="mt-5 min-w-full border border-gray-300">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border-b text-left">Name</th>
+              <th className="py-2 px-4 border-b text-left">Type</th>
+              <th className="py-2 px-4 border-b text-left">Size</th>
+              <th className="py-2 px-4 border-b text-left">Action</th>
+              {/* Add more headers as needed */}
+            </tr>
+          </thead>
+          <tbody>
+            {AllFlower &&
+              AllFlower?.data?.data.map((item: any) => {
+                return (
+                  <tr key={item._id}>
+                    <td className="py-2 px-4 border-b">{item.name}</td>
+                    <td className="py-2 px-4 border-b">{item.type}</td>
+                    <td className="py-2 px-4 border-b">{item.size}</td>
+                    <td className="py-2 px-4 border-b flex gap-2">
+                      <button
+                        onClick={() => {
+                          setDModal(true);
+                          setDModalData(item);
+                        }}
+                        className="px-2 py-1 rounded-md bg-green hover:bg-deeper hover:text-white cursor-pointer transition-all ease-in"
+                      >
+                        Detail
+                      </button>
+                      <button
+                        onClick={() => handleDelUser(item?._id)}
+                        className="px-2 py-1 rounded-md bg-green hover:bg-deeper hover:text-white cursor-pointer transition-all ease-in"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        onClick={() => {
+                          setEditUser(true);
+                          setEUser(item);
+                        }}
+                        className="px-2 py-1 rounded-md bg-green hover:bg-deeper hover:text-white cursor-pointer transition-all ease-in"
+                      >
+                        Edit
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
