f535144fc54f7aceff305ab70c56c5c9c72d5c50
Arko <arkoroyb@gmail.com>
2024-01-27 22:37:22 +0600

    throtling navigation issue solve by using useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useNavigate } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import { useGetUserQuery } from "./redux/features/user/userApi";
+import { setLoggedInfo } from "./redux/features/user/userSlice";
+import { useAppDispatch } from "./redux/hook";
+import { useEffect } from "react";
 
 function App() {
+  const query = useGetUserQuery(undefined);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (query.isSuccess) {
+      dispatch(setLoggedInfo(query.data?.data));
+    }
+    if (query.isError) {
+      dispatch(setLoggedInfo(null));
+      navigate("/login");
+    }
+  }, [query.isSuccess, query.isError, query.data, dispatch, navigate]);
+
+  // Manually trigger refetch when needed
+  // const handleLogout = async () => {
+  //   await query.refetch();
+  //   // Additional logout logic if needed
+  // };
+
   return (
     <>
       <MainLayout />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,50 +1,35 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
-  useGetUserQuery,
   useLogOutMutation,
 } from "../redux/features/user/userApi";
-import { setLoggedInfo } from "../redux/features/user/userSlice";
-import { useAppDispatch, useAppSelector } from "../redux/hook";
+import { useAppSelector } from "../redux/hook";
 
 const Navbar = () => {
-  const {
-    data: InfoData,
-    isSuccess,
-    isError,
-  } = useGetUserQuery(undefined);
+    
   const [logOut] = useLogOutMutation();
 
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
+  
   const { user } = useAppSelector((state) => state.user);
 
-  if (isSuccess) {
-    dispatch(setLoggedInfo(InfoData?.data));
-  }
-
+  
   const handleLogout = async () => {
     await logOut(undefined);
   };
-
-  if (isError) {
-    dispatch(setLoggedInfo(null));
-    navigate("/login");
-  }
 
   return (
     <div className="h-[60px] shadow-md flex justify-end items-center px-10">
       {user.email ? (
         <div className="">
-            <Link
-              className="hover:bg-green px-3 py-1 rounded-md hover:text-white transition-all ease-in"
-              to="login"
-              onClick={()=> handleLogout()}
-            >
-              Logout
-            </Link>
+          <Link
+            className="hover:bg-green px-3 py-1 rounded-md hover:text-white transition-all ease-in"
+            to="login"
+            onClick={() => handleLogout()}
+          >
+            Logout
+          </Link>
         </div>
       ) : (
-        <div className="">
+        <div className="flex">
           <div className="">
             <Link
               className="hover:bg-green px-3 py-1 rounded-md hover:text-white transition-all ease-in"
