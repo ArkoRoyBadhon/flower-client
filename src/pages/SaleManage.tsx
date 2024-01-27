2d6ea80dd32a0f6831733267d3ed71df769fa518
Arko <arkoroyb@gmail.com>
2024-01-27 11:33:52 +0600

    salemanagement page filter option

--- src/pages/SaleManage.tsx
+++ src/pages/SaleManage.tsx
@@ -1,13 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Link } from "react-router-dom";
 import { useGetAllFlowerQuery } from "../redux/features/flower/flowerApi";
+import { useState } from "react";
+import { colorOptions, fragranceOptions, occationOptions, sizeOptions, typeOptions } from "../components/flowerOptions";
+import Select from "react-select";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { stateType } from "../types/addFlowerType";
 
 const SaleManage = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchVal, setSearchVal] = useState("");
+  const [selectedColor, setSelectedColor] = useState<stateType | null>();
+  const [selectedType, setSelectedType] = useState<stateType | null>();
+  const [selectedSize, setSelectedSize] = useState<stateType | null>();
+  const [selectedFragrance, setSelectedFragrance] =
+    useState<stateType | null>();
+  const [selectedOccation, setSelectedOccation] = useState<stateType | null>();
+  const [selectedBloom, setSelectedBloom] = useState<any>("");
+
+
   const {
     data: flowerData,
     isLoading,
     isSuccess,
     error,
-  } = useGetAllFlowerQuery(undefined);
+  } = useGetAllFlowerQuery({
+    searchVal,
+    selectedColor,
+    selectedFragrance,
+    selectedOccation,
+    selectedSize,
+    selectedType,
+    selectedBloom,
+    pageNum:1,
+  });
+  
+  const handleSearch = () => {
+    setSearchVal(searchValue);
+  };
 
   console.log("data", flowerData?.data?.data);
 
@@ -15,12 +46,99 @@ const SaleManage = () => {
     <div className="p-5">
       <h1 className="text-[18px] font-bold text-green w-full">Manage Sale</h1>
       <div className="">
+      <div className="mt-5">
+        <h4 className="">Filter</h4>
         <div className="">
-          <h5 className="">Filter</h5>
+          <div className="h-fit py-5">
+            <div className="flex outline w-[400px] rounded-md outline-1 outline-light">
+              <input
+                className="w-full px-2 outline-none"
+                onChange={(e) => setSearchValue(e.target.value)}
+                type="text"
+                placeholder="Search"
+              />
+              <p
+                onClick={() => handleSearch()}
+                className="bg-light px-2 py-1 rounded-r-md cursor-pointer hover:bg-deep hover:text-white transition-all ease-in"
+              >
+                Search
+              </p>
+            </div>
+            <div className="flex flex-wrap gap-5 w-fit pt-5">
+              <div className="outline outline-1 outline-light px-2 py-[2px] rounded-md flex justify-between items-center gap-5 mb-5">
+                <label htmlFor="selectgender">Color:</label>
+                <Select
+                  name="color"
+                  options={colorOptions}
+                  isClearable={true}
+                  onChange={(selectedOption) =>
+                    setSelectedColor(selectedOption)
+                  }
+                  value={selectedColor}
+                />
+              </div>
+              <div className="outline outline-1 outline-light px-2 py-[2px] rounded-md flex justify-between items-center gap-5 mb-5">
+                <label htmlFor="selectgender">Type:</label>
+                <Select
+                  name="type"
+                  options={typeOptions}
+                  isClearable={true}
+                  onChange={(selectedOption) => setSelectedType(selectedOption)}
+                  value={selectedType}
+                />
+              </div>
+              <div className="outline outline-1 outline-light px-2 py-[2px] rounded-md flex justify-between items-center gap-5 mb-5">
+                <label htmlFor="selectgender">Size:</label>
+                <Select
+                  name="size"
+                  options={sizeOptions}
+                  isClearable={true}
+                  onChange={(selectedOption) => setSelectedSize(selectedOption)}
+                  value={selectedSize}
+                />
+              </div>
+              <div className="outline outline-1 outline-light px-2 py-[2px] rounded-md flex justify-between items-center gap-5 mb-5">
+                <label htmlFor="selectgender">Fragrance:</label>
+                <Select
+                  name="fragrance"
+                  options={fragranceOptions}
+                  isClearable={true}
+                  onChange={(selectedOption) =>
+                    setSelectedFragrance(selectedOption)
+                  }
+                  value={selectedFragrance}
+                />
+              </div>
+              <div className="outline outline-1 outline-light px-2 py-[2px] rounded-md flex justify-between items-center gap-5 mb-5">
+                <label htmlFor="selectgender">Occation:</label>
+                <Select
+                  name="occation"
+                  options={occationOptions}
+                  isClearable={true}
+                  onChange={(selectedOption) =>
+                    setSelectedOccation(selectedOption)
+                  }
+                  value={selectedOccation}
+                />
+              </div>
+
+              <div className="outline outline-1 outline-light px-2 py-[2px] rounded-md flex justify-between items-center gap-5 mb-5">
+                <label htmlFor="selectgender">Bloom Date:</label>
+                <DatePicker
+                  showIcon
+                  // minDate={new Date()}
+                  onChange={(date) => setSelectedBloom(date)}
+                  dateFormat="yyyy-MM-dd"
+                  selected={selectedBloom ? new Date(selectedBloom) : null}
+                />
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
         <hr />
         <div className="mt-5 grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-y-10 md:gap-x-10 gap-x-0">
-          {flowerData?.data?.data?.map((item) => {
+          {flowerData?.data?.data?.map((item:any) => {
             return (
               <div
                 key={item?._id}
@@ -28,8 +146,8 @@ const SaleManage = () => {
               >
                 <div className="w-full h-[50%] overflow-hidden">
                   <img
-                    className="rounded-t-md h-full w-full transition-transform transform hover:scale-110"
-                    src="https://plus.unsplash.com/premium_photo-1667667720425-6972aff75f6b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"
+                    className="rounded-t-md w-full transition-transform transform hover:scale-110 h-[180px] bg-red-500"
+                    src={item?.img}
                     alt="img"
                   />
                 </div>
